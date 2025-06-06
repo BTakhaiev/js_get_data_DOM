6cdcb19fab97ff169f3a7912d54d09b0283f67db
Bohdan <takhaiev@icloud.com>
2025-06-07 00:28:08 +0300

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const population = document.querySelectorAll('.population');
+const populationNum = [];
+
+for (let i = 0; i < population.length; i++) {
+  populationNum.push(Number(population[i].textContent.replace(/,/g, '')));
+}
+
+const sum = populationNum.reduce((acc, value) => acc + value, 0);
+const average = Math.round(sum / populationNum.length);
+const total = document.querySelector('.total-population');
+const aver = document.querySelector('.average-population');
+
+total.textContent = `${sum.toLocaleString('en-US')}`;
+
+aver.textContent = `${average.toLocaleString('en-US')}`;
